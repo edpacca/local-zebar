c5df1febf13622b7c836b21f66bb1f1d22b691b3
Eddie Pace <epace@scottlogic.com>
2025-05-02 12:30:57 +0100

    fix network icon errors

--- src/lib/Network.svelte
+++ src/lib/Network.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-    import type { NetworkGateway, NetworkOutput } from "zebar";
+  import type { NetworkGateway, NetworkOutput } from "zebar";
 
-    export let networkOutput: NetworkOutput | null;
+  export let networkOutput: NetworkOutput;
 
-    const iconNameEthernet = "ethernet-cable";
-    const iconNameWifi = "wifi_strength_";
-    const iconNameNoConnection = "wifi_strength_off_outline";
+  const iconNameEthernet = "ethernet-cable";
+  const iconNameWifi = "wifi_strength";
+  const iconNameNoConnection = "wifi_strength_off_outline";
+  const iconNameError = "wifi_strength_alert_outline";
 
-    let iconClass: string = iconNameNoConnection;
+  $: iconClass = getIconClass(networkOutput);
+  $: ssid = networkOutput?.defaultGateway?.ssid;
+  $: ipaddr = networkOutput?.defaultGateway?.ipv4Addresses
+  ? networkOutput?.defaultGateway?.ipv4Addresses[0]
+  : "";
 
-    $: () => {
-        switch (networkOutput?.defaultInterface?.type) {
-            case "ethernet":
-                iconClass = iconNameEthernet;
-            case "wifi":
-                iconClass = wifiIcon(networkOutput.defaultGateway);
-            default:
-                iconClass = iconNameNoConnection;
-        }
+  const getIconClass = (networkOutput: NetworkOutput) => {
+    switch (networkOutput.defaultInterface?.type) {
+      case "ethernet":
+        return iconNameEthernet;
+      case "wifi":
+        return wifiIcon(networkOutput.defaultGateway);
+      default:
+        return iconNameNoConnection;
     }
+  };
 
-    $: ssid = networkOutput?.defaultGateway?.ssid;
-    $: ipaddr = networkOutput?.defaultGateway?.ipv4Addresses ? networkOutput?.defaultGateway?.ipv4Addresses[0] : "";
-
-    const wifiIconValue = (signalStrength: number | null): string => {
-        if (!signalStrength || signalStrength < 20) {
-            return "outline";
-        } else if (signalStrength > 80) {
-            return "4";
-        } else if (signalStrength > 60) {
-            return "3"
-        } else if (signalStrength > 40) {
-            return "2"
-        } else {
-            return "1"
-        }
+  const wifiIconValue = (signalStrength: number | null): string => {
+    if (!signalStrength || signalStrength < 20) {
+      return "outline";
+    } else if (signalStrength > 80) {
+      return "4";
+    } else if (signalStrength > 60) {
+      return "3";
+    } else if (signalStrength > 40) {
+      return "2";
+    } else {
+      return "1";
     }
+  };
 
-    const wifiIcon = (defaultGateway: NetworkGateway | null) => {
-        if (!defaultGateway) {
-            return `${iconNameWifi}_alert_outline`;
-        }
-        return `${iconNameWifi}_${wifiIconValue(defaultGateway.signalStrength)}`
+  const wifiIcon = (defaultGateway: NetworkGateway | null) => {
+    if (!defaultGateway) {
+      return iconNameError;
     }
+    return `${iconNameWifi}_${wifiIconValue(defaultGateway.signalStrength)}`;
+  };
 </script>
 
-{#if networkOutput}
-    <div class="network">
-        <i class={`nf nf-md-${iconClass}`}></i>
-        {ssid} | {ipaddr}
-    </div>
-{/if}
+<div class="network">
+  <i class={`nf nf-md-${iconClass}`}></i>
+  {ssid} | {ipaddr}
+</div>
