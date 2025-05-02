f6afec76ff07a912478492cb214ebba649e71c15
Eddie Pace <epace@scottlogic.com>
2025-05-02 14:18:56 +0100

    warn icon and text

--- src/lib/IconValue.svelte
+++ src/lib/IconValue.svelte
@@ -4,13 +4,30 @@
     export let isPercentage: boolean = false;
     export let warningValue: number | undefined = undefined;
     export let alertValue: number | undefined = undefined;
+    export let warnAbove = true;
+
+    $: isWarning =
+        warningValue &&
+        ((warnAbove && value >= warningValue) ||
+            (!warnAbove && value <= warningValue));
+
+    $: isAlerting =
+        alertValue &&
+        ((warnAbove && value >= alertValue) ||
+            (!warnAbove && value <= alertValue));
 </script>
 
-<div
-    class:warning={warningValue && value > warningValue}
-    class:alert={alertValue && value > alertValue}
->
-    <i class={iconClass}></i>
-        {value}
-        {#if isPercentage}%{/if}
+<div class="icon-value">
+    <i class={iconClass} class:warn={isWarning} class:alert={isAlerting}></i>
+    <span class:warn={isWarning} class:alert={isAlerting}>
+        {value}{#if isPercentage}%{/if}
+    </span>
 </div>
+
+<style>
+    .icon-value {
+        gap: 2px;
+        display: flex;
+        flex-direction: row;
+    }
+</style>
