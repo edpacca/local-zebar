a6d58fd2495e24a89d0ef748696aa99e37721135
Eddie Pace <eddie@pace.me.uk>
2025-05-09 11:45:26 +0100

    format battery

--- src/lib/Battery.svelte
+++ src/lib/Battery.svelte
@@ -4,20 +4,23 @@
 
     export let battery: BatteryOutput;
 
-    const iconClassIsCharging = "nf nf-md-lightning_bolt charging-icon"
-    $: iconClassChargeAmount = `nf nf-fa-battery_${getBatteryIconValue(battery.chargePercent)}`
+    const iconClassIsCharging = "nf nf-md-lightning_bolt charging-icon";
+    $: iconClassChargeAmount = `nf nf-fa-battery_${getBatteryIconValue(battery.chargePercent)}`;
+
+    const batteryTimeString = (time: number | null): string => {
+        if (time) {
+            return `(${(time / (1000 * 60)).toFixed(0)} min)`;
+        }
+        return "";
+    };
 
     const getBatteryIconValue = (percentage: number) => {
-        if (percentage > 90)
-            return "4";
-        if (percentage > 70)
-            return "3";
-        if (percentage > 40)
-            return "2";
-        if (percentage > 20)
-            return "1";
+        if (percentage > 90) return "4";
+        if (percentage > 70) return "3";
+        if (percentage > 40) return "2";
+        if (percentage > 20) return "1";
         return "0";
-    }
+    };
 </script>
 
 <div class="flex-gap battery">
@@ -32,6 +35,11 @@
         isPercentage={true}
         value={Math.round(battery.chargePercent)}
     />
+    <span class="subtext small"
+        >{batteryTimeString(
+            battery.isCharging ? battery.timeTillFull : battery.timeTillEmpty
+        )}</span
+    >
 </div>
 
 <style>
@@ -44,5 +52,10 @@
         position: absolute;
         font-size: 8px;
         left: -7px;
+        top: -1px;
+    }
+
+    .small {
+        font-size: 10px;
     }
 </style>
