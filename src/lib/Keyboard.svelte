d1e8bbf79dd619f8d6908b88804621ff9bc9715b
Eddie Pace <eddie@pace.me.uk>
2025-07-08 12:19:53 +0100

    WIP keyboard

---
+++ src/lib/Keyboard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let capsLockOn = false;
+
+    const onkeydown = (event: KeyboardEvent) => {
+        capsLockOn = event.getModifierState('CapsLock');
+    }
+</script>
+
+<svelte:window on:keydown={onkeydown}/>
+{#if capsLockOn}
+    <button>CAPS</button>
+{/if}
+
+<style>
+    button {
+        background-color: var(--peach);
+    }
+</style>
