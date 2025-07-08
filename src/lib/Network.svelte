a25c67a6f36c7831b57c808531f514f724460932
Eddie Pace <eddie@pace.me.uk>
2025-07-08 12:19:45 +0100

    tweak network

--- src/lib/Network.svelte
+++ src/lib/Network.svelte
@@ -1,83 +1,102 @@
 <script lang="ts">
-    import type { NetworkGateway, NetworkOutput } from "zebar";
+  import type { NetworkGateway, NetworkOutput } from "zebar";
 
-    export let network: NetworkOutput;
+  export let network: NetworkOutput;
 
-    const iconNameEthernet = "ethernet-cable";
-    const iconNameWifi = "wifi_strength";
-    const iconNameNoConnection = "wifi_strength_off_outline";
-    const iconNameError = "wifi_strength_alert_outline";
-    const iconNameHidden = "nf nf-fa-dungeon";
+  const iconNameEthernet = "ethernet-cable";
+  const iconNameWifi = "wifi_strength";
+  const iconNameNoConnection = "wifi_strength_off_outline";
+  const iconNameError = "wifi_strength_alert_outline";
+  const iconNameHidden = "nf nf-fa-dungeon";
+  // const iconNameTrafficUpDown = "nf nf-fa-arrows_up_down";
 
-    let isHidingIp = false;
+  let isHidingIp = false;
+  $: iconClass = getIconClass(network);
+  $: ssid = network?.defaultGateway?.ssid;
+  $: ipaddr = network?.defaultGateway?.ipv4Addresses
+    ? network?.defaultGateway?.ipv4Addresses[0]
+    : "";
+  $: trafficDown = ((network?.traffic?.received.bytes ?? 0) / 1000).toFixed(0);
+  $: trafficUp = ((network?.traffic?.transmitted.bytes ?? 0) / 1000).toFixed(0);
 
-    $: iconClass = getIconClass(network);
-    $: ssid = network?.defaultGateway?.ssid;
-    $: ipaddr = network?.defaultGateway?.ipv4Addresses
-        ? network?.defaultGateway?.ipv4Addresses[0]
-        : "";
-
-    const getIconClass = (networkOutput: NetworkOutput) => {
-        switch (networkOutput.defaultInterface?.type) {
-            case "ethernet":
-                return iconNameEthernet;
-            case "wifi":
-                return wifiIcon(networkOutput.defaultGateway);
-            default:
-                return iconNameNoConnection;
-        }
-    };
-
-    const wifiIconValue = (signalStrength: number | null): string => {
-        if (!signalStrength || signalStrength < 20) {
-            return "outline";
-        } else if (signalStrength > 80) {
-            return "4";
-        } else if (signalStrength > 60) {
-            return "3";
-        } else if (signalStrength > 40) {
-            return "2";
-        } else {
-            return "1";
-        }
-    };
-
-    const wifiIcon = (defaultGateway: NetworkGateway | null) => {
-        if (!defaultGateway) {
-            return iconNameError;
-        }
-        return `${iconNameWifi}_${wifiIconValue(defaultGateway.signalStrength)}`;
-    };
-
-    const toggleHidingIp = () => {
-        isHidingIp = !isHidingIp;
+  const getIconClass = (networkOutput: NetworkOutput) => {
+    switch (networkOutput.defaultInterface?.type) {
+      case "ethernet":
+        return iconNameEthernet;
+      case "wifi":
+        return wifiIcon(networkOutput.defaultGateway);
+      default:
+        return iconNameNoConnection;
     }
+  };
+
+  const wifiIconValue = (signalStrength: number | null): string => {
+    if (!signalStrength || signalStrength < 20) {
+      return "outline";
+    } else if (signalStrength > 80) {
+      return "4";
+    } else if (signalStrength > 60) {
+      return "3";
+    } else if (signalStrength > 40) {
+      return "2";
+    } else {
+      return "1";
+    }
+  };
+
+  const wifiIcon = (defaultGateway: NetworkGateway | null) => {
+    if (!defaultGateway) {
+      return iconNameError;
+    }
+    return `${iconNameWifi}_${wifiIconValue(defaultGateway.signalStrength)}`;
+  };
+
+  const toggleHidingIp = () => {
+    isHidingIp = !isHidingIp;
+  };
 </script>
 
-<div class="network">
-    <i class={`nf nf-md-${iconClass}`}></i>
-    <button onclick={toggleHidingIp}>
-        {#if isHidingIp}
-        <span><i class={iconNameHidden}></i></span>
-        {:else}
-        {ssid} | <span>{ipaddr}</span>
+<div class="flex-gap">
+  <i class={`nf nf-md-${iconClass}`}></i>
+  <button onclick={toggleHidingIp}>
+    {#if isHidingIp}
+      <span><i class={iconNameHidden}></i></span>
+    {:else}
+      {ssid} | <span>{ipaddr}</span> |
     {/if}
-    </button>
+  </button>
+  <div class="traffic">
+      {trafficUp} / {trafficDown}
+  </div>
 </div>
 
 <style>
-    span {
-        color: var(--subtext0);
-    }
+  span {
+    color: var(--subtext0);
+  }
 
-    button {
-        background: none;
-        border: none;
-        border-radius: 2px;
-    }
+  .flex-gap {
+    gap: 2px;
+  }
 
-    button:hover {
-        background: var(--surface0);
-        color: var(--teal);
-    }
+  .traffic {
+    color: var(--teal);
+    font-size: 8px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 0px;
+  }
+
+  lbutton {
+    background: none;
+    border: none;
+    border-radius: 2px;
+  }
+
+  button:hover {
+    background: var(--surface0);
+    color: var(--teal);
+  }
 </style>
